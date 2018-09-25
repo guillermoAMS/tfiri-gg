842a0e01472d2441db3afee5caf886668aded1ba
guillermoams <ams.guillermo@gmail.com>
2018-09-25 16:42:52 -0500

    New components added. ImageView component missing bounding box functionality

--- tfiri/src/App.js
+++ tfiri/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Toolbar from './components/Toolbar';
+import ImageView from './components/ImageView';
+import LabelList from './components/LabelList';
+
 class App extends Component {
+
+  state = {selectedFile: null}
+
+  handleUploadedImg = (imgPath) => {
+    this.setState({selectedFile: imgPath})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Toolbar uploadedImg={this.handleUploadedImg}/>
+        {
+          this.state.selectedFile && <ImageView key={this.state.selectedFile} uploadedImg={this.state.selectedFile}/>
+        }
+        <LabelList/>
       </div>
     );
   }
